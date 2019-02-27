555d53a979056a166afcbe18c6f7687ef6640edb
Wazier <wazier@Waziers-MacBook-Pro.local>
2019-02-27 22:05:43 +0700

    Update Prettier

--- src/App.js
+++ src/App.js
@@ -1,181 +1,187 @@
 import './App.css';
 
 import {
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  NavItem,
-  NavLink,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  UncontrolledDropdown
+	Collapse,
+	// DropdownItem,
+	// DropdownMenu,
+	// DropdownToggle,
+	Nav,
+	NavItem,
+	NavLink,
+	Navbar,
+	// NavbarBrand,
+	NavbarToggler
+	// UncontrolledDropdown
 } from 'reactstrap';
 import React, { Component } from 'react';
 
 // import logo from './logo.svg';
+var countDownDate = new Date('Jan 5, 2021 15:37:25').getTime();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <>
-      <main role="main" data-reactid=".1zu95ir9q80">
-        <div id="myCarousel" className="carousel slide" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            {/* <li data-target="#myCarousel" data-slide-to="2"></li> */}
-          </ol>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-              <div className="container">
-                <div className="carousel-caption text-left">
-                  <h1>Example headline.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-              <div className="container">
-                <div className="carousel-caption">
-                  <h1>Another example headline.</h1>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                  <p><a className="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+	data_reactid = () => {
+		var text = '.';
+		var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+		for (var i = 0; i < 10; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-         <Navbar color="dark" dark expand="md" className='mb-5'>
-         <div className='container'>
-          {/* <NavbarBrand href="/">Exmo</NavbarBrand> */}
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">Profile</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">Catalogue</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/components/">Submit</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-          </div>
-        </Navbar>
+		return text;
+	};
+	countDown = () => {
+		var x = setInterval(function() {
+			// Get todays date and time
+			var now = new Date().getTime();
 
+			// Find the distance between now and the count down date
+			var distance = countDownDate - now;
 
-        {/* <div className="container marketing">
-          <div className="row">
-            <div className="col-lg-4">
-              <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-              <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div className="col-lg-4">
-              <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-              <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div className="col-lg-4">
-              <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><a className="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-          </div>
+			// Time calculations for days, hours, minutes and seconds
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+			// Display the result in the element with id="demo"
+			// document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+			// + minutes + "m " + seconds + "s ";
+			console.log(days, hours, minutes, seconds);
 
-          
+			// If the count down is finished, write some text
+			if (distance < 0) {
+				clearInterval(x);
+				// return JSON.stringify(days, hours, minutes, seconds);
+				// console.log(days, hours, minutes, seconds);
+				// document.getElementById("demo").innerHTML = "EXPIRED";
+			}
+		}, 1000);
+	};
+	render() {
+		// this.countDown();
+		return (
+			<div>
+				<main role="main" data-reactid={this.data_reactid()} className="vh-100">
+					<div id="myCarousel" className="carousel slide" data-ride="carousel">
+						<ol className="carousel-indicators">
+							<li data-target="#myCarousel" data-slide-to="0" className="active" />
+							<li data-target="#myCarousel" data-slide-to="1" />
+							{/* <li data-target="#myCarousel" data-slide-to="2"></li> */}
+						</ol>
+						<div className="carousel-inner">
+							<div className="carousel-item active">
+								<svg
+									className="bd-placeholder-img"
+									width="100%"
+									height="100%"
+									xmlns="http://www.w3.org/2000/svg"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+									role="img"
+								>
+									<rect width="100%" height="100%" fill="#777" />
+								</svg>
+								<div className="container">
+									<div className="carousel-caption text-left">
+										<h1>Example headline.</h1>
+										<p>
+											Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit
+											non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies
+											vehicula ut id elit.
+										</p>
+										<p>
+											<a className="btn btn-lg btn-primary" href="/" role="button">
+												Sign up today
+											</a>
+										</p>
+									</div>
+								</div>
+							</div>
+							<div className="carousel-item">
+								<svg
+									className="bd-placeholder-img"
+									width="100%"
+									height="100%"
+									xmlns="http://www.w3.org/2000/svg"
+									preserveAspectRatio="xMidYMid slice"
+									focusable="false"
+									role="img"
+								>
+									<rect width="100%" height="100%" fill="#777" />
+								</svg>
+								<div className="container">
+									<div className="carousel-caption">
+										<h1>Another example headline.</h1>
+										<p>
+											Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit
+											non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies
+											vehicula ut id elit.
+										</p>
+										<p>
+											<a className="btn btn-lg btn-primary" href="/" role="button">
+												Learn more
+											</a>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+							<span className="carousel-control-prev-icon" aria-hidden="true" />
+							<span className="sr-only">Previous</span>
+						</a>
+						<a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+							<span className="carousel-control-next-icon" aria-hidden="true" />
+							<span className="sr-only">Next</span>
+						</a>
+					</div>
 
-          <hr className="featurette-divider" />
+					<Navbar color="light" expand="md" className="mb-5">
+						<div className="container">
+							{/* <NavbarBrand href="/">Exmo</NavbarBrand> */}
+							<NavbarToggler onClick={this.toggle} />
+							<Collapse isOpen={this.state.isOpen} navbar>
+								<Nav className="ml-auto" navbar>
+									<NavItem>
+										<NavLink href="/">Home</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="/components/">Profile</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="/components/">Catalogue</NavLink>
+									</NavItem>
+									<NavItem>
+										<NavLink href="/components/">Submit</NavLink>
+									</NavItem>
+								</Nav>
+							</Collapse>
+						</div>
+					</Navbar>
 
-          <div className="row featurette">
-            <div className="col-md-7">
-              <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
-              <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div className="col-md-5">
-              <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
+					<div className="container text-center">
+						<h1 className="big-title">EXMO</h1>
+					</div>
+				</main>
 
-          <hr className="featurette-divider" />
-
-          <div className="row featurette">
-            <div className="col-md-7 order-md-2">
-              <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
-              <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div className="col-md-5 order-md-1">
-              <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
-
-          <hr className="featurette-divider" />
-
-          <div className="row featurette">
-            <div className="col-md-7">
-              <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
-              <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-            </div>
-            <div className="col-md-5">
-              <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-            </div>
-          </div>
-
-          <hr className="featurette-divider" />
-
-          
-
-        </div> */}
-
-
-        <footer className="blog-footer footer mt-auto py-3 fixed-bottom">
-          <p>Template build for Exmo 2019 &copy;.</p>
-          <p>
-            <a href="#">Back to top</a>
-          </p>
-        </footer>
-      </main>
-      </>
-    );
-  }
+				<footer className="blog-footer footer mt-auto py-3">
+					<p>Template build for Exmo 2019 &copy;.</p>
+					<p>Back to top</p>
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
